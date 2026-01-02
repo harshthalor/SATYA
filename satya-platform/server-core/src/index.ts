f564732a6716227a1e7ea95c8bb39cc7813f88b6
905088-nitin <brdkrishxb@gmail.com>
2026-01-02 18:59:13 +0530

    feat: Completed Phase 3 - Full AI Integration and Auth Success

--- satya-platform/server-core/src/index.ts
+++ satya-platform/server-core/src/index.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware (Security & Utilities)
 app.use(cors());                 // Allows Member A (React) to talk to this server
-app.use(express.json());         // Parses incoming JSON data
+app.use(express.json({ limit: '50mb' }));        // Parses incoming JSON data
 app.use('/api/v1/auth', authRoutes);       // Auth Routes (Face Verification)
 app.use('/api/v1/ballot', ballotRoutes);   // Ballot Routes (Fetch Candidates)
 
